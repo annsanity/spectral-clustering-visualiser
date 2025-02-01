6a2fcd3d5ce3a5941051c8af1cae691b5a2d3c94
annsanity <anagha.gulkari@gmail.com>
2025-02-01 16:27:03 +0000

    Changed app.py

--- project_files/backend/app.py
+++ project_files/backend/app.py
@@ -46,7 +46,6 @@ def clustering():
 
     return response
 
-# Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     
