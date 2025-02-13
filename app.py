74e64d9938241732007b29ca6c89ef1bf96fe995
Anagha G. <71192637+annsanity@users.noreply.github.com>
2025-02-13 02:09:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 # Add the backend directory to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
-from backend.algorithms import (
+from project_files.backend.algorithms import (
     agglomerative,
     dbscan,
     kmeans,
