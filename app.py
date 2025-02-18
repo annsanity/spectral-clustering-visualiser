6a8c8765fefdbc21b380c8f702abf477fb9c7962
Anagha G. <71192637+annsanity@users.noreply.github.com>
2025-02-18 19:16:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,10 @@ def clustering():
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
+    
+@app.route("/")
+def home():
+    return "Flask ML App is running!", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
